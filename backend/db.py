a0797577e9f5da12b7e86a4919b500c95819cef2
justina-os <justina.jostle.tech@gmail.com>
2026-08-06 11:56:18 +0530

    preparing for deploying

--- backend/db.py
+++ backend/db.py
@@ -1,16 +1,38 @@
+# import psycopg
+# from psycopg.rows import dict_row
+
+# def get_connection():
+#     con = psycopg.connect(
+#         host="localhost",
+#         dbname="buddy_borrowers",
+#         user="postgres",
+#         password="sql@work",
+#         row_factory=dict_row
+#     )
+
+#     try:
+#         yield con
+#     finally:
+#         con.close()
+
+import os
+
 import psycopg
 from psycopg.rows import dict_row
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 def get_connection():
+
     con = psycopg.connect(
-        host="localhost",
-        dbname="buddy_borrowers",
-        user="postgres",
-        password="sql@work",
+        os.getenv("DATABASE_URL"),
         row_factory=dict_row
     )
 
     try:
         yield con
+
     finally:
         con.close()
--- schema.sql
+++ schema.sql
@@ -1,88 +1,72 @@
--- create TYPE request_status as enum (
---     'requested',
---     'accepted',
---     'rejected',
---     'returned'
--- )
+-- ============ TYPES ============
 
+CREATE TYPE request_status AS ENUM (
+    'requested',
+    'accepted',
+    'rejected',
+    'returned'
+);
 
--- CREATE TYPE status as enum(
---     'available',
---     'rented'
--- )
+CREATE TYPE status AS ENUM (
+    'available',
+    'rented'
+);
 
--- CREATE type category as ENUM(
---     'Object',
---     'Skill'
--- )
+CREATE TYPE category AS ENUM (
+    'Object',
+    'Skill'
+);
 
--- CREATE TABLE resources(
---     resource_id BIGSERIAL PRIMARY KEY,
---     resource_name varchar(255) not NULL,
---     resource_description TEXT,
---     price INTEGER,
---     status status not null ,
---     category category not null,
---     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
---     owner_id BIGINT not null,
---     FOREIGN KEY (owner_id) REFERENCES users(user_id)
--- )
+-- ============ TABLES ============
 
+CREATE TABLE users(
+    user_id BIGSERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    password TEXT NOT NULL,
+    user_name TEXT NOT NULL DEFAULT 'user',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
+CREATE TABLE resources(
+    resource_id BIGSERIAL PRIMARY KEY,
+    resource_name VARCHAR(255) NOT NULL,
+    resource_description TEXT,
+    price INTEGER,
+    status status NOT NULL,
+    category category NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    owner_id BIGINT NOT NULL,
+    FOREIGN KEY (owner_id) REFERENCES users(user_id)
+);
 
--- CREATE table requests(
---     request_id BIGSERIAL PRIMARY KEY,
---     requester_id BIGINT not NULL ,
---     FOREIGN KEY (requester_id) REFERENCES users(user_id),
---     offers TEXT,
---     status request_status not NULL,
---     resource_id BIGINT not NULL,
---     FOREIGN KEY (resource_id) REFERENCES resources(resource_id),
---     requested_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE requests(
+    request_id BIGSERIAL PRIMARY KEY,
+    requester_id BIGINT NOT NULL,
+    lender_id BIGINT NOT NULL,
+    offers TEXT,
+    status request_status NOT NULL,
+    resource_id BIGINT NOT NULL,
+    requested_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (requester_id) REFERENCES users(user_id),
+    FOREIGN KEY (lender_id) REFERENCES users(user_id),
+    FOREIGN KEY (resource_id) REFERENCES resources(resource_id)
+);
 
--- )
+CREATE TABLE messages(
+    msg_id BIGSERIAL PRIMARY KEY,
+    message TEXT NOT NULL,
+    sender_id INT NOT NULL,
+    request_id INT NOT NULL REFERENCES requests(request_id),
+    sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (sender_id) REFERENCES users(user_id)
+);
 
-
--- select * from users;
--- select * from resources;
-
-
--- ALTER TABLE requests
--- ADD COLUMN lender_id BIGINT NOT NULL,
--- ADD CONSTRAINT fk_lender
--- FOREIGN KEY (lender_id)
--- REFERENCES users(user_id);
-
-
--- ALTER TABLE requests
--- DROP COLUMN lender_id;
-
--- delete from users where user_id=1;
--- SELECT * from users;
--- delete from resources where owner_id=1;
-
-
-SELECT * from requests;
-
-
--- create table messages(
---     msg_id BIGSERIAL PRIMARY KEY,
---     message TEXT not null,
---     sender_id int not null ,
---     foreign KEY (sender_id) REFERENCES  users(user_id),
---     request_id INT NOT NULL REFERENCES requests(request_id),
---     sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- );
-
--- CREATE TABLE pending_signup(
---     id BIGSERIAL PRIMARY KEY,
---     mail_id VARCHAR(255) NOT NULL,
---     user_name TEXT NOT NULL,
---     password TEXT NOT NULL,
---     code VARCHAR(6) NOT NULL,
---     expires_at TIMESTAMP DEFAULT (CURRENT_TIMESTAMP + INTERVAL '5 minutes')
--- );
-
-
--- alter TABLE pending_signup
--- add CONSTRAINT unique_mail UNIQUE(mail_id)
+CREATE TABLE pending_signup(
+    id BIGSERIAL PRIMARY KEY,
+    mail_id VARCHAR(255) NOT NULL,
+    user_name TEXT NOT NULL,
+    password TEXT NOT NULL,
+    code VARCHAR(6) NOT NULL,
+    expires_at TIMESTAMP DEFAULT (CURRENT_TIMESTAMP + INTERVAL '5 minutes'),
+    CONSTRAINT unique_mail UNIQUE(mail_id)
+);
